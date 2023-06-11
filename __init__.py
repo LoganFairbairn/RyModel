8c2c888aa431db6c41599075979e337542d3c556
Ryver <58080736+LoganFairbairn@users.noreply.github.com>
2023-06-10 22:17:27 -0400

    Improved cutter selection / hiding.

--- __init__.py
+++ __init__.py
@@ -16,9 +16,10 @@
 # This file imports and registers all required modules for this add-on.
 
 import bpy
+from bpy.app.handlers import persistent
 
 # Import operators.
-from .quick_operators import RyModel_Mirror, RyModel_ResetOrigin, RyModel_AddModifier, RyModel_DeleteModifier, RyModel_CopyModifiers, RyModel_AutoSharpen, RyModel_ExtractFace, RyModel_ExtractCurve, RyModel_CleanMesh, RyModel_SelectNgons, RadialArraySettings, RyModel_RadialArray, RyModel_RemoveRadialArray, RyModel_2xSubDivision, RyModel_CurveToRope, RyModel_Cheshire, RyModel_HSWFModApply, CUTTER_MODE, update_hide_cutters, RyModel_AddCutter, RyModel_Unwrap, RyModel_AutoSeam
+from .quick_operators import RyModel_Mirror, RyModel_ResetOrigin, RyModel_AddModifier, RyModel_DeleteModifier, RyModel_CopyModifiers, RyModel_AutoSharpen, RyModel_ExtractFace, RyModel_ExtractCurve, RyModel_CleanMesh, RyModel_SelectNgons, RadialArraySettings, RyModel_RadialArray, RyModel_RemoveRadialArray, RyModel_2xSubDivision, RyModel_CurveToRope, RyModel_Cheshire, RyModel_HSWFModApply, CUTTER_MODE, update_hide_cutters, hide_cutters, RyModel_AddCutter, RyModel_Unwrap, RyModel_AutoSeam
 
 # Import user interface.
 from .ui_main import RyModel_OT_open_menu, ADDON_VERSION_NUMBER
@@ -88,6 +89,26 @@ def update_boolean_operation(self, context):
                             solidify_modifier = context.active_object.modifiers.get("SliceSolidify")
                             if solidify_modifier:
                                 context.active_object.modifiers.remove(solidify_modifier)
+
+
+def on_active_object_changed():
+    '''Performs updates when the active object is changed.'''
+    if bpy.context.active_object:
+        if bpy.context.active_object.type == 'MESH':
+            if bpy.context.active_object.name.startswith("Cutter_"):
+                hide_cutters()
+
+
+# Mark load handlers as persistent so they are not freed when loading a new blend file.
+@persistent
+def load_handler(dummy):
+    # Subscribe to the active object.
+    subscribe_to = bpy.types.LayerObjects, "active"
+    bpy.types.Scene.matlayer_object_selection_updater = object()
+    bpy.msgbus.subscribe_rna(key=subscribe_to, owner=bpy.types.Scene.matlayer_object_selection_updater, args=(), notify=on_active_object_changed)
+
+# Run startup functions when a new blend file is loaded.
+bpy.app.handlers.load_post.append(load_handler)
 
 addon_keymaps = []
 
